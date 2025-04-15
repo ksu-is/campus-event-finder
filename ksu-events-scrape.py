933134b9f1c895b17f637624be8ba11403901637
winolaflorent <wflorent@students.kennesaw.edu>
2025-04-14 20:57:25 -0400

    Update ksu-events-scrape.py
    started the scrape

--- ksu-events-scrape.py
+++ ksu-events-scrape.py
@@ -1,1 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+url = "https://owllife.kennesaw.edu/events"
+
+page = requests.get(url)
+soup = BeautifulSoup(page.text, "html")
+
+print(soup)
 
